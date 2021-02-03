ff13cc2de7a7f745cfa9b913e5278a7996bfe677
Kayphrosh <olakunbiolabode01@gmail.com>
2021-02-03 05:21:25 +0100

    go

--- app.js
+++ app.js
@@ -174,4 +174,75 @@ hamburger.addEventListener("click", () => {
 });
 
 
-// Creat an element 
+
+const TypeWriter = function(txtElement, words, wait = 30000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
+// TYPE METHOD
+
+TypeWriter.prototype.type = function() {
+    // console.log('heloo');
+    
+    // Current Index Words 
+    const current = this.wordIndex % this.words.length;
+
+    // // Get full text of current word
+    const fullTxt = this.words[current]
+
+    // Check if deleting
+    if(this.isDeleting){
+        //Remove Character
+        this.txt = fullTxt.substring(0, this.txt.length - 14)
+    }
+    else {
+        //Add Character
+        this.txt = fullTxt.substring(0, this.txt.length + 1)
+    }
+    // Insert text into an element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Type Speed
+    let typeSpeed = 500;
+
+    if(this.isDeleting) {
+        typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if(!this.isDeleting && this.txt === fullTxt) {
+        // Make pause at end
+        typeSpeed = this.wait;
+        //Set delete to true
+        this.isDeleting = true;
+    }
+    else if (this.isDeleting && this.txt === '' ){
+        this.isDeleting = false;
+        //Move to the Next word
+        this.wordIndex++;
+        // Pause before start typing
+        typeSpeed = 200;
+    }
+        
+    setTimeout(() => this.type(), 500)
+}
+
+
+// INIT on DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init(){
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    //Init Typewriter
+    new TypeWriter(txtElement, words, wait);
+}
+
